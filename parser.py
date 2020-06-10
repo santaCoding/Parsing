5595fd867bb4b586e6379bfa4e031d27bc19b5d3
santaCoding <landreax100@gmail.com>
2020-06-10 18:26:30 +0300

    parser education.ua universes odessa DONE

--- parser.py
+++ parser.py
@@ -10,15 +10,18 @@ def get_inner_info(url):
     response = requests.get(url)
     if response.status_code == 200:
         html_inner = BeautifulSoup(response.text, 'html.parser')
-        heading = html_inner.find('h1', class_='heading')
-        trs = html_inner.find('table', class_='w620').findAll('tr')
-        print(trs)
+        heading = html_inner.find('h1', itemprop="name")
+        print(heading.get_text())
+        trs = html_inner.find('table', class_=('tbl_info', 'nf')).findAll('tr')
+        #print(trs)
         tds = []
         for tr in trs:
-            for td in tr:
-                tds.append(td)
+            for td in tr.findAll('td', class_=None):
+                if '\xa0' not in td:
+                    tds.append(td.get_text().replace(' ', '').replace('\r', '').replace('Показать', ''))
         print('-'*20)
         print(tds)
+        return heading.get_text(), tds
 
     else:
         print(f'error, not 200 for {url}')
@@ -28,7 +31,12 @@ def get_page(url):
     if response.status_code == 200:
         html_soup = BeautifulSoup(response.text, 'html.parser')
         a_list = ['https://www.education.ua' + x['href'] for x in html_soup.findAll('a', class_=('viplink', 'h4_link'))]
-        get_inner_info
+        with open('text.csv', 'w', newline='') as csv_f:
+            write = csv.writer(csv_f)
+            write.writerow(('Название', 'Город', 'Год основания', 'Статус', 'Аккредитация', 'Документ об окончании', 'Форма обучения', 'Квалиф. уровни', 'Адрес', 'Телефон', 'Телефон приемное комиссии', 'Сайт'))
+            for a in a_list:
+                heading, data = get_inner_info(a)
+                write.writerow((heading, data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10]))
     else:
         print('error, not 200')
 
