1a22dce629a6e723a182dbae98c413b2a04f2649
santaCoding <landreax100@gmail.com>
2020-06-10 17:38:50 +0300

    .

--- parser.py
+++ parser.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from time import sleep
 from random import randint
-import
+import csv
 
 headers = ({'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'})
 
@@ -10,10 +10,13 @@ def get_inner_info(url):
     response = requests.get(url)
     if response.status_code == 200:
         html_inner = BeautifulSoup(response.text, 'html.parser')
-        heading = html_inner.findAll('h1', class_='heading')
-        vmiddle = html_inner.findAll('td', class_='vmiddle')
-        tables = html_inner.findAll('table', class_='w620')
-        return heading[0].get_text(), vmiddle[0].get_text(), tables[0].get_text()
+        heading = html_inner.find('h1', class_='heading')
+        print(heading.get_text())
+        trs = html_inner.find('table', class_='w620').findAll('tr')
+        print(trs)
+        for tr in trs:
+            for td in tr:
+                print(td)
     else:
         print(f'error, not 200 for {url}')
 
@@ -25,13 +28,14 @@ def get_page(url):
         a_list = []
         for tr in range(len(trs)):
             a_list.append('https://ru.osvita.ua' + trs[tr].a['href'])
-        with open('text.csv', 'w') as csv_file:
-            for page in a_list:
-                heading_text, vmiddle_text, tables_text = get_inner_info(page)
-                write = csv.writer(csv_file)
-                write.writerow(heading_text)
-                write.writerow(vmiddle_text)
-                write.writerow(tables_text)
+        print(a_list)
+        #with open('text.csv', 'w') as csv_file:
+            #write = csv.writer(csv_file)
+            #write.writerow(['Название', 'что-то там'])
+        for page in a_list:
+                get_inner_info(page)
+                #write.writerow(heading_text)
+                #write.writerow(tables_text)
     else:
         print('error, not 200')
 
