30388f88ed95ddd138ddebb24a2429524ccd1823
santaCoding <landreax100@gmail.com>
2021-02-03 20:08:23 +0200

    .

--- dynamic/parser.py
+++ dynamic/parser.py
@@ -13,16 +13,14 @@ def findI(url, driver):
     driver.get(url)
     print(url)
     sleep(2)
-    dhtml = driver.find_element_by_css_selector('body').get_attribute('outerHTML')
-    btn = driver.find_element_by_css_selector('li.order-8-l.gray5')
-    btn.click()
-    dwn = driver.find_element_by_css_selector('div.ph5.pv4.bg-gray1.br3.br--bottom')
+    dwn = driver.find_element_by_class_name('svg-inline--fa.fa-download.fa-w-18')
     dwn.click()
-    sleep(3)
+    sleep(7)
 
 
 try:
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "a")))
+    sleep(4)
     while True:
         try:
             btn = driver.find_element_by_class_name('mt3-ns')
@@ -37,8 +35,8 @@ try:
     soup = BeautifulSoup(pageBody, 'html.parser')
     a_list = ['https://fontawesome.com' + x['href'] for x in soup.findAll('a')]
 finally:
+    a_list.pop(0)
     print('\n\n', len(a_list), '\n\n')
-    for a in range(len(a_list)):
-        print(a+1)
-        findI(a_list[a], driver)
+    for a in a_list:
+        findI(a, driver)
     driver.quit()
